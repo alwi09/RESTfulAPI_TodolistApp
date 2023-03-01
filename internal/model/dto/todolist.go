7b800e4357d1da1d2b87ac48ade1dcac0684f709
alwi irfani <alwi.irfani1927@gmail.com>
2023-03-01 22:59:26 +0700

    feat: add file create todo request and update todo request

---
+++ internal/model/dto/todolist.go
@@ -0,0 +1,27 @@
+package dto
+
+type CreateTodolistRequest struct {
+	Title string `json:"title" binding:"required,min=2"`
+	Description string `json:"description" binding:"required.min=4"`
+}
+
+type UpdateTodolistRequest struct {
+	Title string `json:"title" binding:"required,min=2"`
+	Description string `json:"description" binding:"required,min=4"`
+	Status bool `json:"status"`
+}
+
+func (request *UpdateTodolistRequest) RequestUpdateTodolist() map[string]interface{} {
+	update := make(map[string]interface{}, 0)
+	if request.Title != "" {
+		update["title"] = request.Title
+	}
+
+	if request.Description != "" {
+		update["description"] = request.Description
+	}
+
+	update["status"] = request.Status
+
+	return update
+}
