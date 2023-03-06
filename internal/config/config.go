e83a9855b30c728c96199b5e580bbba1336b6ccc
alwi irfani <alwi.irfani1927@gmail.com>
2023-03-06 21:24:58 +0700

    docs: change db_user and db_name

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,10 @@
 package config
 
 type Config struct {
-	DBDriver string `envconfig:"DB_DRIVER" default:"mysql"`
-	DBUser string `envconfiig:"DB_USER" default:"root"`
-	DBPassword string `envconvig:"DB_PASSWORD" required:"true"`
-	DBHost string `envconvig:"DB_HOST" default:"localhost"`
-	DBPort int `envconvig:"DB_PORT" default:"1234"`
-	DBName string `envconvig:"DB_NAME" default:"todolist"`
+	DBDriver   string `envconfig:"DB_DRIVER" default:"mysql"`
+	DBUser     string `envconfiig:"DB_USER" default:"alwi09"`
+	DBPassword string `envconvig:"DB_PASSWORD" default:"alwiirfani091199"`
+	DBHost     string `envconvig:"DB_HOST" default:"localhost"`
+	DBPort     int    `envconvig:"DB_PORT" default:"3306"`
+	DBName     string `envconvig:"DB_NAME" default:"todolist"`
 }
