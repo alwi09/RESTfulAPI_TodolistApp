dccaf3af326f3fb1af870b82c9c19efe996ca9c0
alwi irfani <alwi.irfani1927@gmail.com>
2023-03-06 21:37:59 +0700

    docs: tidy up the rows

--- internal/model/entity/todolist.go
+++ internal/model/entity/todolist.go
@@ -1,8 +1,8 @@
 package entity
 
 type Todos struct {
-	Id int64 `gorm:"primaryKey" json:"id"`
-	Title string `gorm:"type:varchar(99)" json:"title"`
+	Id          int64  `gorm:"primaryKey" json:"id"`
+	Title       string `gorm:"type:varchar(99)" json:"title"`
 	Description string `gorm:"type:varchar(999)" json:"description"`
-	Status bool `gorm:"default:false" json:"status"`
+	Status      bool   `gorm:"default:false" json:"status"`
 }
