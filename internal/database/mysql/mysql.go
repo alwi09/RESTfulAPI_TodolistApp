d4e8dd603fe4a3ea1b797d58882557784c64f323
alwi irfani <alwi.irfani1927@gmail.com>
2023-03-06 21:29:14 +0700

    feat: add setting sqlDB

--- internal/database/mysql/mysql.go
+++ internal/database/mysql/mysql.go
@@ -17,15 +17,15 @@ import (
 )
 
 func Connect(ctx context.Context, cfg *config.Config) (*gorm.DB, error) {
-	
+
 	fmt.Printf("%+v\n ", cfg)
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-			cfg.DBUser,
-			cfg.DBPassword,
-			cfg.DBHost,
-			cfg.DBPort,
-			cfg.DBName,
+		cfg.DBUser,
+		cfg.DBPassword,
+		cfg.DBHost,
+		cfg.DBPort,
+		cfg.DBName,
 	)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
@@ -33,8 +33,8 @@ func Connect(ctx context.Context, cfg *config.Config) (*gorm.DB, error) {
 			log.New(os.Stdout, "\r\n", log.LstdFlags),
 			logger.Config{
 				SlowThreshold: time.Second,
-				LogLevel: logger.Info,
-				Colorful: true,
+				LogLevel:      logger.Info,
+				Colorful:      true,
 			},
 		),
 	})
@@ -48,7 +48,25 @@ func Connect(ctx context.Context, cfg *config.Config) (*gorm.DB, error) {
 		logrus.Error(err)
 	}
 
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+	sqlDB.SetMaxIdleConns(10)
+
+	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	sqlDB.SetMaxOpenConns(100)
+
+	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	if err := sqlDB.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
 	logrus.Info("Connect database successfully")
 
-	return db,err
+	return db, err
 }
