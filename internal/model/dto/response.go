7af0e4c3ca0b7a33396d585c4033406f88631549
alwi irfani <alwi.irfani1927@gmail.com>
2023-03-10 01:08:32 +0700

    feat: add response for todolist

---
+++ internal/model/dto/response.go
@@ -0,0 +1,33 @@
+package dto
+
+import "todolist_gin_gorm/internal/model/entity"
+
+type TodolistResponseCreate struct {
+	Status  int         `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+type TodolistResponseGetAll struct {
+	Status  int            `json:"status"`
+	Message string         `json:"message"`
+	More    int            `json:"more"`
+	Data    []entity.Todos `json:"data"`
+}
+
+type TodolistResponseGetID struct {
+	Status  int          `json:"status"`
+	Message string       `json:"message"`
+	Data    entity.Todos `json:"data"`
+}
+
+type TodolistResponseDelete struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+}
+
+type TodolistResponseUpdate struct {
+	Status  int         `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
