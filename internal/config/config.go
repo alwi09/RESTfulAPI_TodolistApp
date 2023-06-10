649101a7e2c0c495d31909d3fd30bddef587da77
alwi09 <alwiirfani1927@gmail.com>
2023-06-10 09:41:35 +0700

    docs: fix typo

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,7 @@ package config
 
 type Config struct {
 	DBDriver   string `envconfig:"DB_DRIVER" default:"mysql"`
-	DBUser     string `envconfiig:"DB_USER" default:"alwi09"`
+	DBUser     string `envconfig:"DB_USER" default:"alwi09"`
 	DBPassword string `envconvig:"DB_PASSWORD" default:"alwiirfani091199"`
 	DBHost     string `envconvig:"DB_HOST" default:"localhost"`
 	DBPort     int    `envconvig:"DB_PORT" default:"3306"`
