311d433ef80e5ac4329487a59319c258423b60f8
alwi09 <alwiirfani1927@gmail.com>
2023-06-12 20:37:14 +0700

    docs: add column:todos_id

--- internal/model/entity/todolist.go
+++ internal/model/entity/todolist.go
@@ -1,7 +1,7 @@
 package entity
 
 type Todos struct {
-	Id          int64  `gorm:"primaryKey" json:"id"`
+	Id          int64  `gorm:"primaryKey;column:todos_id" json:"id"`
 	Title       string `gorm:"type:varchar(99)" json:"title"`
 	Description string `gorm:"type:varchar(999)" json:"description"`
 	Status      bool   `gorm:"default:false" json:"status"`
