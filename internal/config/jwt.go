87aa86a2dab45722ed7b17a61e8de991243738f8
alwi09 <alwiirfani1927@gmail.com>
2023-06-13 09:03:49 +0700

    docs: delete command

--- internal/config/jwt.go
+++ internal/config/jwt.go
@@ -6,41 +6,7 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
-// // untuk sign-in token
-// var JWTSecretKey = []byte("secret-key")
-
-// // payload token
-// type Claims struct {
-// 	Email string `json:"email"`
-// 	jwt.StandardClaims
-// }
-
-// create token
-// func CreateJWTToken(email string) (string, error) {
-// 	// mengatur waktu kadalwarsa token
-// 	expirationTime := time.Now().Add(24 * time.Hour)
-
-// 	// membuat Claims
-// 	claims := &Claims{
-// 		Email: email,
-// 		StandardClaims: jwt.StandardClaims{
-// 			ExpiresAt: expirationTime.Unix(),
-// 		},
-// 	}
-
-// 	// create token dengan sign-in method HS256 dan secret-key
-// 	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
-
-// 	// sign token dengan secret-key
-// 	tokenString, err := token.SignedString(JWTSecretKey)
-
-// 	if err != nil {
-// 		return "", err
-// 	}
-
-// 	return tokenString, nil
-// }
-
+// untuk sign-in token
 const (
 	jwtSecretKey       = "secret-key"
 	jwtExpirationHours = 24
