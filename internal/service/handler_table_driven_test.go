886a089b25585e539a5fbf00198338d013bc3569
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2023-07-11 19:36:42 +0700

    feat: add add table driven testing to get all and create todo

--- internal/service/handler_table_driven_test.go
+++ internal/service/handler_table_driven_test.go
@@ -1,9 +1,11 @@
 package service
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,10 +15,12 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRunTableDriven(t *testing.T) {
 	t.Run("TestTableDrivenGetAllTodolist", TestTableDrivenGetAllTodolist)
+	t.Run("TestTableDrivenCreateTodolist", TestTableDrivenCreateTodolist)
 }
 
 func TestTableDrivenGetAllTodolist(t *testing.T) {
@@ -104,6 +108,95 @@ func TestTableDrivenGetAllTodolist(t *testing.T) {
 			assert.Equal(t, test.expextedResponse.Message, respon.Message)
 			assert.Equal(t, test.expextedResponse.More, respon.More)
 			assert.Equal(t, test.expextedResponse.Data, respon.Data)
+		})
+	}
+}
+
+func TestTableDrivenCreateTodolist(t *testing.T) {
+
+	testCase := []struct {
+		name           string
+		bodyRequest    string
+		mock           func(repository *mocks.Repository)
+		expectedStatus int
+		expectedData   entity.Todos
+		expectedError  string
+	}{
+		{
+			name:        "success",
+			bodyRequest: `{"title": "sholat", "description": "sholat tahajud"}`,
+			mock: func(mock *mocks.Repository) {
+				newTodo := &entity.Todos{
+					Title:       "sholat",
+					Description: "sholat tahajud",
+					Status:      false,
+				}
+				mock.On("Create", "sholat", "sholat tahajud").Return(newTodo, nil)
+			},
+			expectedStatus: http.StatusCreated,
+			expectedData: entity.Todos{
+				Title:       "sholat",
+				Description: "sholat tahajud",
+				Status:      false,
+			},
+			expectedError: "",
+		},
+		{
+			name:           "invalid validation",
+			bodyRequest:    `{"title": "", "description": ""}`,
+			mock:           func(mock *mocks.Repository) {},
+			expectedStatus: http.StatusBadRequest,
+			expectedData:   entity.Todos{},
+			expectedError:  "invalid input validation",
+		},
+		{
+			name:        "internal server error",
+			bodyRequest: `{"title": "sholat", "description": "sholat tahajud"}`,
+			mock: func(mock *mocks.Repository) {
+				expectedErr := errors.New("internal server error")
+				mock.On("Create", "sholat", "sholat tahajud").Return(nil, expectedErr)
+			},
+			expectedStatus: http.StatusInternalServerError,
+			expectedData:   entity.Todos{},
+			expectedError:  "internal server error",
+		},
+	}
+
+	for _, test := range testCase {
+		t.Run(test.name, func(t *testing.T) {
+			mockRepo := mocks.NewRepository(t)
+			test.mock(mockRepo)
+			handler := NewHandlerImpl(mockRepo)
+
+			point := "/create_todolist"
+
+			reqBody := bytes.NewBufferString(test.bodyRequest)
+			req, err := http.NewRequest(http.MethodPost, point, reqBody)
+			require.NoError(t, err)
+
+			recorder := httptest.NewRecorder()
+			ctx, router := gin.CreateTestContext(recorder)
+			router.POST(point, handler.CreateHandlerTodolist)
+
+			ctx.Request = req
+			router.ServeHTTP(recorder, req)
+
+			responBody, err := io.ReadAll(recorder.Body)
+			require.NoError(t, err)
+
+			if test.expectedError != "" {
+				var errResponse dto.ErrorResponse
+				err = json.Unmarshal(responBody, &errResponse)
+				require.NoError(t, err)
+				assert.Equal(t, test.expectedError, errResponse.Message)
+			} else {
+				var result dto.TodolistResponseCreate
+				err = json.Unmarshal(responBody, &result)
+				require.NoError(t, err)
+				assert.Equal(t, test.expectedData, result.Data)
+			}
+
+			assert.Equal(t, test.expectedStatus, recorder.Code)
 		})
 	}
 }
