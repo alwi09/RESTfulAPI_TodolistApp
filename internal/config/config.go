7b8afdf4c3a4a0c87ec7535e82f26e36d4fcf7fd
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2023-07-12 16:18:11 +0700

    docs: slight change to run local

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,7 +4,7 @@ type Config struct {
 	DBDriver   string `envconfig:"DB_DRIVER" default:"mysql"`
 	DBUser     string `envconfig:"DB_USER" default:"alwi09"`
 	DBPassword string `envconfig:"DB_PASSWORD" default:"alwiirfani091199"`
-	DBHost     string `envconfig:"DB_HOST" default:"mysql-mycontainer"`
+	DBHost     string `envconfig:"DB_HOST" default:"localhost"`
 	DBPort     int    `envconfig:"DB_PORT" default:"3306"`
-	DBName     string `envconfig:"DB_NAME" default:"todolist_restfulapi"`
+	DBName     string `envconfig:"DB_NAME" default:"todolist"`
 }
