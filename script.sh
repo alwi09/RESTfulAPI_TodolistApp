6a74ae7686b483c38984c11a5a467c6643228db0
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2023-07-21 20:20:11 +0700

    feat: add binary file ./cmd/main.go

--- script.sh
+++ script.sh
@@ -3,3 +3,8 @@ sudo docker build -t alwi09/todolist-image:v1 .
 
 ## how to install mockery
 go install github.com/vektra/mockery/v2@latest
+
+## how to build binary file restfulAPI todo
+go build -o myApp ./cmd/main.go
+## cara run
+./myApp
